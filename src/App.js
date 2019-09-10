b38c1626599441ca2f2f0a11fd6800668f343207
Barakat Turki <barakat.turki@gmail.com>
2019-09-11 00:13:43 +0200

    update layout on new branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './App.css'
 function App() {
   return (
     <div className="App">
-      <h2>React</h2>
+      <h1>Daslance</h1>
+      <h2>Visit me at <a href="https://daslance.com" target="_blank">Das Lance labe</a></h2>
     </div>
   );
 }
